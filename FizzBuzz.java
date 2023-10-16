30dc739dc02fb4e084310b89682990b17fa1d1c2
Gaglani <shriyu.gaglani@fiserv.com>
2023-10-16 16:22:52 -0400

    Added FizzBuzz solution

---
+++ FizzBuzz.java
@@ -0,0 +1,58 @@
+public class FizzBuzz{
+    public void fizzBuzz(int n){
+        for(int i = 1; i <= n; i++){
+            if(i % 3 == 0){
+                if(i % 5 == 0){
+                    System.out.println("FizzBuzz");
+                }
+                else{
+                    System.out.println("Fizz");
+                }
+            }
+            else if(i % 5 == 0){
+                if(i % 3 == 0){
+                    System.out.println("FizzBuzz");
+                }
+                else{
+                    System.out.println("Buzz");
+                }
+            }
+            else{
+                System.out.println(i);
+            }
+        }
+    }
+    public void recursiveFizz(int n,int temp){
+        if(n % 3 == 0){
+            if(n % 5 == 0){
+                System.out.println("FizzBuzz");
+            }
+            else{
+                System.out.println("Fizz");
+            }
+            }
+            else if(n % 5 == 0){
+                if(n % 3 == 0){
+                    System.out.println("FizzBuzz");
+                }
+                else{
+                    System.out.println("Buzz");
+                }
+            }
+            else{
+                System.out.println(n);
+            }
+            if(temp > 0){
+                n++;
+                temp--;
+                recursiveFizz(n,temp);
+            }
+        
+
+    }
+    public static void main(String[] args) {
+        FizzBuzz fizz = new FizzBuzz();
+        fizz.fizzBuzz(15);
+        fizz.recursiveFizz(1,14);
+    }
+}
